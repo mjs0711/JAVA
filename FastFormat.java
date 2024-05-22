537145153296dec35adc7671517675cef4f5160f
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-05-22 12:45:05 +0900

    Create FastFormat.java

---
+++ FastFormat.java
@@ -0,0 +1,19 @@
+package s21212_00;
+
+public class FastFormat {
+
+	public static void main(String[] args) {
+		  System.out.printf("이름 : %s","우리들 ");
+	      System.out.printf("학번 : %d\n",21211);
+	      
+	      System.out.printf("이름 : %s"+ "","우리들, ");
+	      System.out.printf("학번 : %d ",21211);
+	      System.out.printf("전화번호 %s\n","010-1234-5678");
+	      
+	      System.out.printf("이름 : %s"+ "","우리들, ");
+	      System.out.printf("전화번호 %s","010-1234-5678 ");
+	      System.out.printf("학번 : %d\n ",21211);
+
+	}
+
+}
