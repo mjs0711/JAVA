d0603be89228acef5de7189a8a204d937cfc11a2
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-05-22 12:49:53 +0900

    Create S21212_01.java

---
+++ S21212_01.java
@@ -0,0 +1,14 @@
+package s21212_00;
+
+public class S21212_01 {
+
+	public static void main(String[] args) {
+		int intNum1 =0;
+		if (intNum1 ==0) {
+			int intNum2 = 10;
+			System.out.println("Num1:" + intNum1 + " if Num2:" + intNum2);
+		}
+
+	}
+
+}
