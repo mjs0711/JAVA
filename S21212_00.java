f71f2b0b06f530c3b371e25a5a028c8666f3ae90
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-05-22 12:48:03 +0900

    Create S21212_00.java

---
+++ S21212_00.java
@@ -0,0 +1,16 @@
+package s21212_00;
+
+public class S21212_00 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int cnt = 1; cnt < 11 ; cnt ++) {
+			
+			sum = sum + cnt; 
+		}
+		
+		System.out.println("sum : " + sum); 
+		
+	}
+
+}
