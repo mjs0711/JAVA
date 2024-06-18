6ae0e920af38ab05acdf39e35017420eb8e1734b
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-06-18 09:39:53 +0900

    Create Chap01.java

---
+++ Chap01.java
@@ -0,0 +1,22 @@
+package s21212_java10;
+import s21212_java11.Matiz;
+
+public class Chap01 {
+	public static void main(String[] args) {
+		
+		Animal am = new Animal();
+		am.a();
+		
+		Dog d = new Dog();
+		d.a();
+		
+		Fish f = new Fish();
+		f.a();
+		
+		Bird bd = new Bird();
+		bd.a();
+		
+		Matiz m = new Matiz();
+		m.a();									
+	}
+}
