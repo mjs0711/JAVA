c5967226766e79fc0fd20565c55b4a1437c3f700
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-06-19 09:41:12 +0900

    Create chap01.java

---
+++ chap01.java
@@ -0,0 +1,12 @@
+package s21212_java20;
+
+public class chap01 {
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.pub(); 
+		s.pro();
+		s.def();
+		//s.pri();//private에러발생
+	}
+
+}
