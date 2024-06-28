9b4a6aa218494aa87ff885057c2662e5c8be0540
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-06-28 10:46:46 +0900

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,22 @@
+select * from tab;
+
+select * from pc_emp;
+
+select * from pc_menu;
+
+insert into PC_MENU values( 10, '음료', '체리콕', 3000);
+
+select max(menu_no) from PC_MENU;
+
+select * from pc_emp;
+
+insert int PC_EMP values( 5, '민지성', 'slave', '2007-07-11', 1045678978, '경기도 성남시', 100);
+
+insert into PC_EMP values(?,?,?,?,?,?,?.?);
+
+-- PC_MENU 테이블에서 메뉴명(menu_name) '제로콜라'의
+-- 가격(menu_price)을 2500원으로 변경하시오.
+
+-- pdate PC_MENU
+-- set menu_price = 2500
+-- where menu_name '제로콜라';
