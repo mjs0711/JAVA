44f89f817047a7cfc6bf679eedc3b17a3d0adf95
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-07-11 10:24:16 +0900

    Create CarExample.java

---
+++ CarExample.java
@@ -0,0 +1,25 @@
+package ch6.Test1;
+
+class Car {
+	// 속성
+	String company = "현대자동차";
+	String model = "제네시스G80";
+	String color = "검정";
+	int speed = 180;
+	
+	Car() {};
+	
+	Car(int x) {};
+}
+public class CarExample {
+	public static void main(String[] args) {
+		// 객체 생성 후 참조 변수에 저장
+		Car myCar = new Car();
+		
+		myCar.model = "그랜져";
+		System.out.println("제작회사: " + myCar.company);
+		System.out.println("모델명: " + myCar.model);
+		System.out.println("컬러: " + myCar.color);
+		System.out.println("최대속도: " + myCar.speed);
+	}
+}
