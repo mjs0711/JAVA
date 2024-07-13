8f4998eecbb61feb124e8f8f175ae266ddcc9303
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-07-14 02:59:26 +0900

    Create class StaticTest2.java

---
+++ class StaticTest2.java
@@ -0,0 +1,24 @@
+package ch6.Test6;
+
+class After2 {
+	// 클래스 변수
+	static int cntStatic = 50;
+	int cnt = 70;
+	String name = "방과후2차";
+	After2(String name, int cnt) {
+		cntStatic++;
+		System.out.println(name + " cnt:" + cnt + " cntStatic:" + cntStatic);
+	}
+}
+public class StaticTest2 {
+	public static void main(String[] args) {
+		After2 n1 = new After2("A", 10);
+		After2 n2 = new After2("B", 20);
+		After2 n3 = new After2("C", 30);
+		
+		After2.cntStatic = 700;
+		System.out.println(n1.name + " cnt:" + n1.cnt + " cntStatic:" + n1.cntStatic);
+		System.out.println(n2.name + " cnt:" + n2.cnt + " cntStatic:" + n2.cntStatic);
+		System.out.println(n3.name + " cnt:" + n3.cnt + " cntStatic:" + n3.cntStatic);
+	}
+}
