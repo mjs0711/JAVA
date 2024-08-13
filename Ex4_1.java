71d2db998d1af818197d9cc79112ce5699a3e871
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-08-14 00:09:10 +0900

    Create Ex4_1.java

---
+++ Ex4_1.java
@@ -0,0 +1,13 @@
+package zlast.t1;
+
+public class Ex4_1 {
+	public static void main(String[] args) {
+		int score = 60;
+		
+		if (score > 60) {
+			System.out.println("합격입니다");
+		} else {
+			System.out.println("불합격입니다");
+		}
+	}	
+}
