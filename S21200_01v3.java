7f61abbde96bc2bdf720a7207558f811df5440e9
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-08-28 15:39:18 +0900

    Create S21200_01v3.java

---
+++ S21200_01v3.java
@@ -0,0 +1,14 @@
+package s21200_java00;
+
+public class S21200_01v3 {
+	public static void main(String[] args) {
+
+		boolean stop = true;
+
+		if (stop) {
+			System.out.println("운행을 중단합니다");
+		} else {
+			System.out.println("운행을 시작합니다");
+		}
+	}
+}
