52e00cf136f5a7d148556413a750f977b3e4da6d
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-08-29 18:08:02 +0900

    Create s21200_01v4.java

---
+++ s21200_01v4.java
@@ -0,0 +1,11 @@
+package s21200_java;
+
+public class s21200_01v4 {
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		double result = x/y;
+		
+		System.out.println("result :" +result);
+	}
+}
