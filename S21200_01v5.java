f8f619dd780158aa9ebceee1b0455ca923544657
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-09-03 17:33:34 +0900

    Create S21200_01v5.java

---
+++ S21200_01v5.java
@@ -0,0 +1,10 @@
+package s21212;
+
+import java.io.IOException;
+
+public class S21200_01v5 {
+	public static void main(String[] args) throws IOException {
+		int keycode = System.in.read();
+		System.out.println("keycode:" + keycode);
+	}
+}
