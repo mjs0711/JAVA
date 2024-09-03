85176698e2440d60140f4024ca0ccea685d037d7
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-09-03 17:02:00 +0900

    Create printf.java

---
+++ printf.java
@@ -0,0 +1,19 @@
+package s21212;
+
+public class printf {
+	public static void main(String[] args) {
+		int value= 123;
+		System.out.printf("상품의 가격: %d원\n", value);
+		System.out.printf("상품의 가격: %6d원\n", value);
+		System.out.printf("상품의 가격: %-6d원\n", value);
+		System.out.printf("상품의 가격: %06d원\n", value);
+		
+		double area = 3.14159 * 10 * 10;
+		System.out.printf("반지름의 %d인 원의 넓이:%10.2f\n", 10, area);
+		
+		String name = "우리들";
+		String job = "소프트웨어개발과";
+		System.out.printf("%6d | %-10s | %10s \n", 1, name, job);
+	}
+
+}
