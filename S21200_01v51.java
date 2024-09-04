c4634afbc89b0684f38304558d76319ee9561f26
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-09-04 16:45:53 +0900

    Create S21200_01v51.java

---
+++ S21200_01v51.java
@@ -0,0 +1,19 @@
+package s21212;
+
+import java.io.IOException;
+
+public class S21200_01v51 {
+	public static void main(String[] args) throws IOException {
+		int keycode;
+		
+		keycode = System.in.read();
+		System.out.println("keycode: " + keycode);
+		
+		keycode = System.in.read();
+		System.out.println("keycode: " + keycode);
+		
+		keycode = System.in.read();
+		System.out.println("keycode: " + keycode);	
+	}
+
+}
