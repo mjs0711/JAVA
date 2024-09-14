be5450e80d5e43a55aa8992508b358df551cea18
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-09-14 16:07:39 +0900

    Create S21200_03.java

---
+++ S21200_03.java
@@ -0,0 +1,20 @@
+package after;
+
+public class S21200_03 {
+	public static void main(String[] args) {
+		int input = 0;
+		
+		if (input == 0) {
+			System.out.println("a : 값은 0 입니다");
+		}
+		if (input == 0) {
+			System.out.println("a : 값은 0이 아닙니다");
+		}
+		
+		if (input == 0) {
+			System.out.println("b : 값은 0 입니다");
+		} else {
+			System.out.println("b : 값은 0이 아닙니다");
+		}
+	}
+}
