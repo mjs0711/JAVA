a8169dfb77e88190ae47a6c3ef7c75e7947ca610
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-10-16 07:49:28 +0900

    Create S21200_03v2.java

---
+++ S21200_03v2.java
@@ -0,0 +1,27 @@
+package s21200;
+
+import java.util.Scanner;
+
+public class S21200_03v2 {
+	public static void main(String[] args) {
+		System.out.println("현재 월을 입력하세요>");
+		Scanner sc = new Scanner(System.in);
+		int month = sc.nextInt();
+		switch(month) {
+			case 3:
+			case 4:
+			case 5: 
+				System.out.println("현제의 계절은 봄입니다");
+				break;
+			case 6: case 7: case 8:
+				System.out.println("현제의 계절은 여름입니다");
+				break;
+			case 9: case 10: case 11:
+				System.out.println("현제의 계절은 가을입니다");
+				break;
+			default:
+				System.out.println("현제의 계절은 겨울입니다");
+			}
+										
+		}
+	}
