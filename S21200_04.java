dd9d1f3bd237ff3d3f0ee1fb95ead369f3a121b5
민지성 <127863368+mjs0711@users.noreply.github.com>
2024-10-18 07:39:25 +0900

    Create S21200_04.java

---
+++ S21200_04.java
@@ -0,0 +1,18 @@
+package s21200;
+
+public class S21200_04 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("매개변수의 개수:"+args.length);
+		
+		int k = 3;
+		for (int i=0; i<args.length; i++) {
+			System.out.println("args[" + i + "] = \"" + args[i] + "\"");
+			
+			//Run>Configuration>탭Arguments
+			
+			//Program argument
+			//2학년12반12번 민지성 성일정보고등학교 학생
+		}
+	}
+}
